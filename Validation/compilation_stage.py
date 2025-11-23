45ee7020b3312a7c9991470ed514e2805193cb7d
Ryan Pettes <ryan.pettes@gmail.com>
2025-11-22 21:15:25 -0500

    Added functionality to extract `.c`, `.h` files from source directory.
    Completed method of `ValidationPipeline` class to be able to sift through the directory containing all LLM-generated files, correctly extracting `.c` files into a list, `.h` files into a list, and a `build_tool` if one is present.
    
    Also added `__init.py__` to the `Validation/` directory to make the folder a valid Python package.
    
    Misc. minor changes made to `compilation_stage.py`. Still not functional.

--- Validation/compilation_stage.py
+++ Validation/compilation_stage.py
@@ -1,13 +1,26 @@
-from typing import Dict, Any
+from typing import List, Dict, Any
 
 class CompilationStage:
-    """Runs the compilation stage of the validation pipeline"""
+    """Class to run the compilation stage of the validation pipeline"""
 
-    def __init__(self, output_dir: str)
+    def __init__(self, output_dir: str):
         """
-        Initialize the compilation stage
+        Initialize the compilation stage with where to write stage's output
         """
         self.output_dir = output_dir
 
-        # Container to collect LLM-generated code files from output directory
-        self.
+    def run(self, source_files: List[str], header_files: List[str], build_tool: str = None) -> Dict[str, Any]:
+        """
+        Run the compilation stage of validation pipeline
+
+        Returns:
+            bb
+        """
+        # CASE 1: No build tool provided
+        if not build_tool:
+            self.run_manual_compile(source_files)
+
+        # CASE 2: Build tool provided
+        else:
+            # self.run_via_build_tool()
+            pass
