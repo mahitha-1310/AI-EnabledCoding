2af5880ba5a76ba1daa8148bebbfb7a7aea9cd39
Max Khouvongsavanh <max2025@vt.edu>
2026-04-27 13:58:08 -0400

    moved test projects into `tests` folder

--- _validation_test.py
+++ _validation_test.py
@@ -4,8 +4,9 @@ import json
 
 # If testing LLMMetric make sure to add the sub directory as well.
 test_dir = input("Enter the `Test<#>/` directory to test: ")
+path = os.path.join("tests", "ValidationTests", test_dir)
 
-if (not os.path.exists("ValidationTests/" + test_dir)) or (not test_dir.strip()):
+if (not os.path.exists(path)) or (not test_dir.strip()):
     print(f"No directory named \"{test_dir}\" found.")
     exit()
 
@@ -15,8 +16,8 @@ if "Test5/" in test_dir:
     prompt = "Write a C function called checksum8 that computes an 8-bit checksum over an input buffer. The function should accept a pointer to unsigned bytes and a buffer length. It should return the sum of all bytes modulo 256."
 
 val_pipe = ValidationPipeline(
-    output_dir="ValidationTests/" + test_dir,
-    source_dir="ValidationTests/" + test_dir,
+    output_dir=path,
+    source_dir=path,
     prompt=prompt
 )
 
